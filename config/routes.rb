f1bef3444429adb45dd69b301cdaecfb6ffeca0c
Carson Cole <carson.cole@gmail.com>
2022-10-25 13:06:21 -0700

    routing updated for admin users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
   get '/about' => 'about#index', as: 'about'
 
   resources :reservations, except: [:index] do
