3a6e31b761e214cbcf5deacc86b37dc45d1956af
Carson Cole <carson.cole@gmail.com>
2022-10-26 10:54:15 -0700

    added mailer for reservations

---
+++ app/mailers/reservations_mailer.rb
@@ -0,0 +1,2 @@
+class ReservationsMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/reservations_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/reservations_mailer
+class ReservationsMailerPreview < ActionMailer::Preview
+
+end
