7eea86f753b59c5466b1658d3db9d493e4c15647
Carson Cole <carson.cole@gmail.com>
2022-10-27 14:08:27 -0700

    initial confirmation mailer added

--- app/mailers/reservations_mailer.rb
+++ app/mailers/reservations_mailer.rb
@@ -1,3 +1,9 @@
 #TODO send email on confirmation of reservation
 class ReservationsMailer < ApplicationMailer
+
+  def confirmed_reservation
+    @reservation = params[:reservation]
+    @url  = 'http://example.com/login'
+    mail(to: @reservation.email, subject: 'Your trip pickup is confirmed')
+  end
 end
