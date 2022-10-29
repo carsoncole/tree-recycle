ce7e9822e51c57a344b8a8bac9c04d07a3304001
Carson Cole <carson.cole@gmail.com>
2022-10-29 16:13:20 -0700

    initial confirmation email added

--- app/mailers/reservations_mailer.rb
+++ app/mailers/reservations_mailer.rb
@@ -4,6 +4,6 @@ class ReservationsMailer < ApplicationMailer
   def confirmed_reservation
     @reservation = params[:reservation]
     @url  = 'http://example.com/login'
-    mail(to: @reservation.email, subject: 'Your trip pickup is confirmed')
+    mail(to: @reservation.email, subject: 'Your tree pickup is confirmed')
   end
 end
