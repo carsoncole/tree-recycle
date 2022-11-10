46951209c46ff910aa626467e78367740700fced
Carson Cole <carson.cole@gmail.com>
2022-11-09 23:08:28 -0800

    geocodable concern added

---
+++ app/models/concerns/geocodable.rb
@@ -0,0 +1,17 @@
+module Geocodable
+  extend ActiveSupport::Concern
+
+  included do
+
+    def address
+      [street, city, state, country].compact.join(', ')
+    end
+
+    def coordinates
+      if geocoded?
+        [latitude, longitude]
+      end
+    end
+
+  end
+end
