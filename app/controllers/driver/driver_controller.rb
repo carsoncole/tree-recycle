90a662d350e4ecf6ee3f3e691f372942f948bbae
Carson Cole <carson.cole@gmail.com>
2022-11-17 10:30:56 -0800

    added site_title

--- app/controllers/driver/driver_controller.rb
+++ app/controllers/driver/driver_controller.rb
@@ -1,5 +1,10 @@
 class Driver::DriverController < ApplicationController
   before_action :is_driver_signed_in?
+  before_action :set_title
+
+  def set_title
+    @site_title = 'Tree Recycle Driver'
+  end
 
   def is_driver_signed_in?
     unless driver_signed_in?
