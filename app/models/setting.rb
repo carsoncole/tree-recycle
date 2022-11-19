1ed121bfde09f49c13cb9cb263ad0825ef3a6ca9
Carson Cole <carson.cole@gmail.com>
2022-11-19 09:24:29 -0800

    refactored app settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,10 +1,5 @@
 # Application and event day defaults, and hosting organization information is stored in the first record of this model.
 class Setting < ApplicationRecord
-  attribute :site_title, :string, default: 'Tree Recycle'
-  attribute :organization_name, :string, default: 'Troop 100'
-  attribute :default_city, :string, default: 'Bainbridge Island'
-  attribute :default_state, :string, default: 'Washington'
-  attribute :default_country, :string, default: 'United States'
   attribute :pickup_date_and_time, :datetime, default: -> { Time.now + 1.month }
   attribute :sign_up_deadline_at, :datetime, default: -> { Time.now + 1.month - 1.day }
   attribute :contact_name, :string, default: 'John Doe'
---
+++ config/initializers/constants.rb
@@ -0,0 +1,5 @@
+SITE_TITLE = 'Tree Recycle'
+ORGANIZATION_NAME = 'Troops 1564 & 1804'
+DEFAULT_CITY = 'Bainbridge Island'
+DEFAULT_STATE = 'Washington'
+DEFAULT_COUNTRY = 'United States'
