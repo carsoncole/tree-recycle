99c078a9e1cbf0b154b1cb2539188a8afa833ead
Carson Cole <carson.cole@gmail.com>
2022-11-24 08:40:36 -0800

    added log to donation receipt

--- app/mailers/donations_mailer.rb
+++ app/mailers/donations_mailer.rb
@@ -11,9 +11,11 @@ class DonationsMailer < ApplicationMailer
 
   def receipt_email
     @donation = params[:donation]
+    @reservation = @donation.reservation
     return if @donation.is_receipt_email_sent
     mail(to: @donation.email, subject: 'Thank you for your donation')
     @donation.update(is_receipt_email_sent: true) if setting.is_emailing_enabled
+    @reservation.logs.create(message: 'Donation receipt email sent.') if @reservation
   end
 
 end
