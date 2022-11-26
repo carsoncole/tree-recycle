531d12dc8136280d4dbecb090c71944b03693589
Carson Cole <carson.cole@gmail.com>
2022-11-26 13:46:55 -0800

    AWS geocoding added

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem "faker"
 gem "factory_bot_rails"
 gem "bugsnag", "~> 6.24"
 gem 'twilio-ruby', '~> 5.73.3'
+gem 'aws-sdk-locationservice'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,6 +2,15 @@ Geocoder.configure(
   # Geocoding options
   timeout: 10,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
+  lookup: :amazon_location_service,
+  amazon_location_service: {
+    index_name: 'tree_recycle',
+    api_key: {
+      access_key_id: Rails.application.credentials.lookups.amazon_location_service.access_key_id,
+      secret_access_key: Rails.application.credentials.lookups.amazon_location_service.secret_access_key
+    }
+  }
+
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
@@ -25,3 +34,6 @@ Geocoder.configure(
   #   prefix: 'geocoder:'
   # }
 )
+
+
+Aws.config.update({region: 'us-west-2'})
