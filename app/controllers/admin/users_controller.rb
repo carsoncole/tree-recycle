c665daac694a7f59570774de433a99652aa27ca7
Carson Cole <carson.cole@gmail.com>
2022-11-27 22:07:17 -0800

    [FEATURE] Dropdown role added

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,13 +4,26 @@ class Admin::UsersController < Clearance::UsersController
   layout 'admin/admin'
 
   def index
-    @users = User.all
+    @users = User.order(:email)
+    @roles = User.roles.map {|key, value| [key.titleize, key] }.compact
   end
+
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_url, notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :phone)
+      params.require(:user).permit(:email, :password, :phone, :role)
     end
 
     def require_administrator
