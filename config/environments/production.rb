c1f01b8cb16f445557b6cc69f34ad7e7fcb19153
Carson Cole <carson.cole@gmail.com>
2022-11-29 07:19:23 -0800

    change asset source to treerecyle.org

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = "https://www.treerecycle.net"
+  config.asset_host = "https://www.treerecycle.org"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
