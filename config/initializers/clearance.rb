27968b650b9aedc8c0fcd1e69703543a592124ca
Carson Cole <carson.cole@gmail.com>
2022-12-02 13:11:47 -0800

    disable new user signups

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,6 @@
 Clearance.configure do |config|
   config.routes = false
-  config.allow_sign_up = true
+  config.allow_sign_up = false
   config.mailer_sender = "Bainbrdge Tree Recycle <troop_1564@treerecycle.net>"
   config.rotate_csrf_on_sign_in = true
   config.redirect_url = '/admin'
