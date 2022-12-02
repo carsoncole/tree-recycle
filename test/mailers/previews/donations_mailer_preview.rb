de80d29975a61776e43205ad857addba4b15206b
Carson Cole <carson.cole@gmail.com>
2022-12-01 16:59:40 -0800

    [fix] minor styling

--- test/mailers/previews/donations_mailer_preview.rb
+++ test/mailers/previews/donations_mailer_preview.rb
@@ -2,7 +2,7 @@
 class DonationsMailerPreview < ActionMailer::Preview
 
   def receipt_email
-    Donation.first.update(is_receipt_email_sent: false)
+    Donation.first_or_create.update(is_receipt_email_sent: false)
     donation = Donation.first
     DonationsMailer.with(donation: donation).receipt_email
   end
