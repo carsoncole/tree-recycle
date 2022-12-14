fd9b3f595fbec224baa216083b62a7407f6dd11d
Carson Cole <carson.cole@gmail.com>
2022-12-13 19:41:01 -0800

    [fix] minor

--- app/controllers/driver/reservations_controller.rb
+++ app/controllers/driver/reservations_controller.rb
@@ -14,15 +14,15 @@ class Driver::ReservationsController < Driver::DriverController
     flash[:notice] = "Reservation status changes are NOT enabled." unless helpers.setting.is_driver_site_enabled?
     if params[:route_id]
       @route = Route.find(params[:route_id])
-      @reservations = Reservation.pending.where(route: @route ).geocoded.map{ |r| [ r.id.to_s, r.street.to_s, r.latitude.to_s.to_f, r.longitude.to_s.to_f, r.notes || "", r.status.humanize.capitalize, r.phone || "", r.donation || "", r.collected || "", r.collected_amount || "", r.name] }
+      @reservations = Reservation.pending.where(route: @route ).geocoded.map{ |r| [ r.id.to_s, r.street.to_s, r.latitude.to_s.to_f, r.longitude.to_s.to_f, r.notes || "", r.status.humanize.capitalize, r.phone || "", r.donation || "", r.collected || "", r.collected_amount || "", r.name || ""] }
     elsif params[:zone_id]
       @zone = Zone.find(params[:zone_id])
-      @reservations = @zone.reservations.pending_pickup.geocoded.map{ |r| [ r.id.to_s, r.street.to_s, r.latitude.to_s.to_f, r.longitude.to_s.to_f, r.notes || "", r.status.humanize.capitalize, r.phone || "", r.donation || "", r.collected || "", r.collected_amount || "", r.name] }
+      @reservations = @zone.reservations.pending_pickup.geocoded.map{ |r| [ r.id.to_s, r.street.to_s, r.latitude.to_s.to_f, r.longitude.to_s.to_f, r.notes || "", r.status.humanize.capitalize, r.phone || "", r.donation || "", r.collected || "", r.collected_amount || "", r.name || ""] }
     elsif params[:reservation_id]
       @reservation = Reservation.find(params[:reservation_id])
-      @reservations = [ @reservation.id.to_s, @reservation.street.to_s, @reservation.latitude.to_s.to_f, @reservation.longitude.to_s.to_f, @reservation.notes || "", @reservation.status.humanize.capitalize, @reservation.phone || "", @reservation.donation || "", @reservation.collected || "", @reservation.collected_amount || "", @reservation.name]
+      @reservations = [ @reservation.id.to_s, @reservation.street.to_s, @reservation.latitude.to_s.to_f, @reservation.longitude.to_s.to_f, @reservation.notes || "", @reservation.status.humanize.capitalize, @reservation.phone || "", @reservation.donation || "", @reservation.collected || "", @reservation.collected_amount || "", @reservation.name || ""]
     else
-      @reservations = Reservation.pending_pickup.geocoded.map{ |r| [ r.id.to_s, r.street.to_s, r.latitude.to_s.to_f, r.longitude.to_s.to_f, r.notes || "", r.status.humanize.capitalize, r.phone || "", r.donation || "", r.collected || "", r.collected_amount || "", r.name] }
+      @reservations = Reservation.pending_pickup.geocoded.map{ |r| [ r.id.to_s, r.street.to_s, r.latitude.to_s.to_f, r.longitude.to_s.to_f, r.notes || "", r.status.humanize.capitalize, r.phone || "", r.donation || "", r.collected || "", r.collected_amount || "", r.name || ""] }
     end
   end
 
