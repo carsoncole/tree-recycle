7589c93ee2e78b6abf76d956dffa158369838664
Carson Cole <carson.cole@gmail.com>
2022-12-22 08:39:14 -0800

    [fix] router tests; coordinates on route form

--- test/factories/routes.rb
+++ test/factories/routes.rb
@@ -19,5 +19,26 @@ FactoryBot.define do
       latitude { nil }
       longitude { nil }
     end
+    
+    factory :route_battlepoint do 
+      name { 'Battlepoint' }
+      street { '9091 Olympus Beach Rd NE' }
+      latitude { 47.646477104939216 }
+      longitude { -122.5810297797926 }
+    end
+
+    factory :route_winslow_north do 
+      name { 'Winslow North' }
+      street { '657 Annie Rose Ln NW' }
+      latitude { 47.630273714285714 }
+      longitude { -122.52775357142858 }
+    end
+
+    factory :route_high_school do 
+      name { 'High School' }
+      street { '9458 Capstan Dr NE' }
+      latitude { 47.640161093604064 }
+      longitude { -122.53038800087906 }
+    end
   end
 end
---
+++ test/models/admin/router_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class Admin::RouterTest < ActiveSupport::TestCase
+
+  test "routing a reservation" do 
+    route_battlepoint = create(:route_battlepoint)
+    reservation = create(:reservation, street: '1760 Susan Place' )
+
+    # should route to the only 1 existing route
+    assert_equal route_battlepoint, reservation.route
+    assert_equal 2.51, reservation.distance_to_route.round(2)
+
+    # should route to the newer closer route
+    route_winslow = create(:route_winslow_north)
+    reservation.route!
+    reservation.save
+    assert_equal 0.68, reservation.distance_to_route.round(2)
+    assert_equal route_winslow, reservation.reload.route
+
+    # should route to the newer even closer route
+    route_high_school = create(:route_high_school)
+    reservation.route!
+    reservation.save
+    assert_equal route_high_school, reservation.reload.route
+  end
+
+end
