7efe4b9031df5043a0a54081df61ad5ba30d3d27
Carson Cole <carson.cole@gmail.com>
2022-12-27 08:32:59 -0800

    [fix] minor

--- app/controllers/admin/donations_controller.rb
+++ app/controllers/admin/donations_controller.rb
@@ -9,6 +9,7 @@ class Admin::DonationsController < Admin::AdminController
 
   def send_donation_receipt
     donation = Donation.find(params[:donation_id])
+    donation.update(is_receipt_email_sent: false)
     donation.send_receipt_email!
     redirect_to admin_reservation_url(donation.reservation)
   end
