a92ed07c0b4543397c6a71e6f464bf941c43b553
Carson Cole <carson.cole@gmail.com>
2023-01-03 20:35:22 -0800

    [fix] email count added to settings

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,4 +2,8 @@ class Log < ApplicationRecord
   belongs_to :reservation
 
   after_create_commit -> { broadcast_prepend_to "logs", partial: "admin/logs/log" }
+
+  def self.email_count_last_24_hours
+    where(message: ['Donation receipt email sent.', 'Confirmed reservation email sent.', 'Marketing email 2 sent.' ]).where("created_at > ?", Time.now - 24.hours).count
+  end
 end
