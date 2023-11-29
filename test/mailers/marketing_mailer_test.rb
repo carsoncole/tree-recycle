345bf9820c199dfe879cc350d115eea8c8e2a264
Carson Cole <carson.cole@gmail.com>
2023-11-29 11:18:25 -0800

    Fixed number of years doing event

--- test/mailers/marketing_mailer_test.rb
+++ test/mailers/marketing_mailer_test.rb
@@ -12,7 +12,7 @@ class MarketingMailerTest < ActionMailer::TestCase
     # assert_equal ["me@example.com"], email.from
     assert_equal [reservation.email], email.to
     assert_equal "Tree recycling #{nice_long_date_short(Time.parse(EVENT_DATE_AND_PICKUP_TIME))}. Register today.", email.subject
-    assert email.html_part.body.to_s.include? "It's our 26th year of recycling Christmas trees on Bainbridge"
+    assert email.html_part.body.to_s.include? "We've picked up up your tree in the past, and if you would like us to pick up again this year, please let us know by registering on our website"
   end
 
   test "marketing email 2" do
