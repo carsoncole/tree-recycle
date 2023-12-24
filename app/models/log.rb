e26fd11b759a7aa51d225b50f025bd046b8318b2
Carson Cole <carson.cole@gmail.com>
2023-12-24 11:02:32 -0800

    fix: add marketing email 1 to filter of last 24 hour emails

--- app/models/log.rb
+++ app/models/log.rb
@@ -4,7 +4,7 @@ class Log < ApplicationRecord
   after_create_commit -> { broadcast_prepend_to "logs", partial: "admin/logs/log" }
 
   def self.email_count_last_24_hours
-    where(message: ['Donation receipt email sent.', 'Confirmed reservation email sent.', 'Marketing email 2 sent.', 'Pick up reminder email sent.' ]).where("created_at > ?", Time.now - 24.hours).count
+    where(message: ['Donation receipt email sent.', 'Confirmed reservation email sent.', 'Marketing email 1 sent.', 'Marketing email 2 sent.', 'Pick up reminder email sent.' ]).where("created_at > ?", Time.now - 24.hours).count
   end
 end
 
