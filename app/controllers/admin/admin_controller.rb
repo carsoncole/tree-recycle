8e4628e2460ab910c06ec1e05d670f860cb6b6c5
Carson Cole <carson.cole@gmail.com>
2024-01-05 13:41:27 -0800

    feat: add status table to admin index for tracking status during day of pickups

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,5 +3,8 @@ class Admin::AdminController < ApplicationController
 
   def index
     @missing_reservations = Reservation.missing
+    @count_picked_up = Reservation.picked_up.count
+    @count_pending_pickups = Reservation.pending_pickup.count
+    @count_missing = Reservation.missing.count
   end
 end
